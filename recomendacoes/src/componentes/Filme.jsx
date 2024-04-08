22ac42047e1036b5471732472d39c97d438b55a8
murilopbc <murilopoltronieri3@gmail.com>
2024-04-08 16:09:37 -0300

    commit

--- recomendacoes/src/componentes/Filme.jsx
+++ recomendacoes/src/componentes/Filme.jsx
@@ -1,15 +1,14 @@
 import estilos from './Filme.module.css'
-import poster from "../assets/poster.jpg";
 import { Card } from './Card';
 
-export function Filme() {
+export function Filme({propsFilme}) {
     return (
         <Card>
             <figure className={estilos.container}>
-                <img src={poster} alt="" />
-                <figcaption>Duna: Parte 2</figcaption>
-                <p>"Duna: Parte Dois" continua a explorar a viagem de Paul Atreides que agora se une a Chani e aos Fremen para vingar a conspiração que destruiu a sua família. Ao enfrentar uma escolha entre o amor e o destino do universo, lutará para evitar o futuro terrível que só ele pode prever.</p>
-                <p>84</p>
+                <img src={`https://image.tmdb.org/t/p/w400${propsFilme.poster_path}`} alt="" />
+                <figcaption>{propsFilme.title}</figcaption>
+                <p>{propsFilme.overview}</p>
+                <p>{propsFilme.vote_average}</p>
             </figure>
 
         </Card>
--- recomendacoes/src/paginas/Lista.jsx
+++ recomendacoes/src/paginas/Lista.jsx
@@ -1,10 +1,23 @@
 import estilos from './Lista.module.css'
 import {Filme} from '../componentes/Filme'
+import { useEffect, useState } from 'react'
 
 export function Lista() {
+
+    const  [filmes, setFilmes] = useState([])
+
+    const apiKey = "07e9b188ce58e608d62827e85ec07ed5"
+
+    useEffect( () => {
+        fetch(`https://api.themoviedb.org/3/discover/movie?api_key=${apiKey}&sort_by=popularity.desc&language=pt-BR`)
+        .then(resposta => resposta.json())
+        .then(dados => setFilmes(dados.results))
+    }, [])
+
     return (
         <main className={estilos.container}>
-            <Filme/>
+            {filmes.map( umFilme => <Filme propsFilme={umFilme} />)}
+            
         </main>
     )
 }
